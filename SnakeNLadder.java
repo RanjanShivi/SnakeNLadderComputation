eedca0218a8513abc193745633b8fff1f65da807
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-09 18:15:28 +0530

    Snake and Ladder game played with single player at start position 0

---
+++ SnakeNLadder.java
@@ -0,0 +1,8 @@
+public class SnakeNLadder {
+    public static void main(String[] args) {
+        int pos = 0;
+        System.out.println("The player's starting position= " + pos);
+
+    }
+    }
+
