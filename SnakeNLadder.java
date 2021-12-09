c87bcaf5479c748362a3dcf76062c678acad85a0
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-09 18:44:43 +0530

    Repeat till winning postion is 100 and restart from 0 is postion comes below 0

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -2,21 +2,29 @@ public class SnakeNLadder {
     public static void main(String[] args) {
         int pos = 0;
         System.out.println("The player's starting position= " + pos);
-        int die = (int) ((Math.random() * 10) % 6 ) + 1;
-        System.out.println("the number in die = " + die);
 
-        int option = (int) (Math.random() * 10 ) % 3 ;
-        switch (option) {
-            case 1:
-                pos += die;
-                System.out.println("Ladder : position= " + pos);
-                break;
-            case 2:
-                pos -= die;
-                System.out.println("Snake : position= " + pos);
-                break;
-            default:
-                System.out.println("No Case : positiion= " + pos);
+        while(pos<100) {
+            int die = (int) ((Math.random() * 10) % 6 ) + 1;
+            System.out.println("the number in die = " + die);
+            int option = (int) (Math.random() * 10) % 3;
+            switch (option) {
+                case 1:
+                    pos += die;
+                    System.out.println("Ladder : position= " + pos);
+                    break;
+                case 2:
+                    if((pos-die) < 0) {
+                        pos = 0;
+                        System.out.println("Snake : position" + pos);
+                    }
+                    else {
+                        pos -= die;
+                        System.out.println("Snake : position= " + pos);
+                    }
+                    break;
+                default:
+                    System.out.println("No Case : positiion= " + pos);
+            }
         }
     }
     }
