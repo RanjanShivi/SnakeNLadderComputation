ed70eda7589c1e1628523521d71f69eca9d5574c
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-09 18:21:25 +0530

    Player rolled the die to get number between 1 and 6

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -2,6 +2,8 @@ public class SnakeNLadder {
     public static void main(String[] args) {
         int pos = 0;
         System.out.println("The player's starting position= " + pos);
+        int die = (int) ((Math.random() * 10) % 6 ) + 1;
+        System.out.println("the number in die = " + die);
 
     }
     }
